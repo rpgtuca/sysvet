cdb5439ad70e4a39400f0e3b644bec923af4fd4e
aluno <aluno@ubuntu-test>
2018-03-03 10:20:50 -0300

    Instalação do Symfony

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -17,5 +18,15 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+    /**
+     * @Route("/contato")
+     */
+    
+    public function contatoAction(Request $request){
+        $nome = $request->get("nome");
+             
+        
+       return $this->render('default/contato.html.twig', array("nome" => $nome));
     }
 }
